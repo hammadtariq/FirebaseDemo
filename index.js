524df9b15046cc2d18ec166e6be2f8311ea862b2
hammad tariq <hammy-786@live.com>
2015-07-10 17:32:25 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * Created by 192.168.3.49 on 7/10/2015.
+ */
+var app = angular.module('myApp',['firebase']);
+
+app.controller('homeController',function($firebaseObject,$firebaseArray){
+
+    var ref= new Firebase('https://asim.firebaseio.com/').child('/abc');
+    //var abc = $firebaseObject(ref);
+    /*this.func = function () {
+        abc.email = this.email;
+        abc.password = this.password;
+        abc.$save();
+    };*/
+   // console.log(abc.email);
+    this.messages = $firebaseArray(ref);
+     var vm=this;
+    this.addMessage = function() {
+        this.messages.$add(
+            {
+                name:this.email,
+                age:this.password
+            });
+    };
+    vm.abc = $firebaseArray(ref);
+    console.log(vm.abc);
+
+});
